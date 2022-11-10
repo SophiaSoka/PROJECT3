41f19022b11da4c415487062a8399fb0cb0f8b4a
Sophia Soka <112036430+SophiaSoka@users.noreply.github.com>
2022-11-10 11:30:05 -0700

    all getters and setters added

--- Trap.h
+++ Trap.h
@@ -11,14 +11,25 @@ class Trap{
         string answer;
         string hint;
         bool hasFound;
-        bool hasSolved;
-
+        bool solved;
     public:
         Trap();
         Trap(string text, string answer, string hint);
+        
+        string getRiddle();
+        void setRiddle(string new_riddle);
 
-        void readTraps(string filename);
+        string getAnswer();
+        void setAnswer(string new_answer);
 
+        string getHint();
+        void setHint(string new_hint);
+
+        bool getFound();
+        void wasFound();
+
+        bool getSolved();
+        void wasSolved();
 
 };
 #endif
