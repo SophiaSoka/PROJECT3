cac902de9d9733175261111c40018f4caaede5e2
Sophia Soka <112036430+SophiaSoka@users.noreply.github.com>
2022-11-10 12:02:39 -0700

    done

--- Player.cpp
+++ Player.cpp
@@ -2,43 +2,43 @@
 
 using namespace std;
 
-Player:: Player (){
+Player::Player (){
     strength = 100;
     lives = 1;
     weapon = "";
     num_hints = 0;
 }
-Player:: Player(int lives, string weapon, int hint){
+Player::Player(int lives, string weapon, int hint){
     strength = 100;
     lives = lives;
     weapon = weapon;
     num_hints = hint;
 }
 
-void Player:: reduceStrength(int takeAwayStrength){
+void Player::reduceStrength(int takeAwayStrength){
     strength = strength - takeAwayStrength;
 }
 int Player::getStrength(){
     return strength;
 }
 
-void Player:: removeWeapon(){
+void Player::removeWeapon(){
     weapon = "";
 }
-string Player:: getWeapons(){
+string Player::getWeapons(){
     return weapon;
 }
 
-void Player:: setLives(int l){
+void Player::setLives(int l){
     lives = l;
 }
-int Player:: getLives(){
+int Player::getLives(){
     return lives;
 }
 
-void Player:: setNumHints(int h){
+void Player::setNumHints(int h){
     num_hints = h;
 }
-int Player:: getNumHints(){
+int Player::getNumHints(){
     return num_hints;
 }
