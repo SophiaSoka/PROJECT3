f46703002bfc894957cac60c98c5da9eda5411b4
make3831 <117610230+make3831@users.noreply.github.com>
2022-11-10 15:59:01 -0700

    comments added

--- Trap.h
+++ Trap.h
@@ -6,6 +6,8 @@
 using namespace std;
 
 class Trap{
+    // We had to declare the variables we declared prior as a public so they could be manipulated by anyone 
+    // We set the other variables as setters to set the variables value in my function and these are voided, so they will not return
     private:
         string riddle;
         string answer;
@@ -13,7 +15,9 @@ class Trap{
         bool hasFound;
         bool solved;
     public:
-        Trap();
+        // We used getters and setters in our h file
+        // Declared the variables inside the parameter
+        Trap(); // default constructor
         Trap(string text, string answer, string hint);
         
         string getRiddle();
