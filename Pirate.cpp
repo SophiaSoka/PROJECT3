fbca994dd180bd31ca0fc50d10580dd96b2310e9
make3831 <117610230+make3831@users.noreply.github.com>
2022-11-10 13:39:11 -0700

    Updated pirate.cpp with comments

--- Pirate.cpp
+++ Pirate.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// default constructor to set the initial values equal to 50, 0, and false
 Pirate::Pirate()
 {
     strength = 50;
@@ -11,7 +12,8 @@ Pirate::Pirate()
     positionC = 0;
     beenFound = false;
 }
-
+// Here, we had to declare all of my variables as their type
+// This was used to organize the information
 Pirate::Pirate(int s, int r, int c)
 {
     strength = s;
@@ -19,7 +21,8 @@ Pirate::Pirate(int s, int r, int c)
     positionC = c;
     beenFound = false;
 }
-
+// We used getters and setters to retrieve the data from the h files we made
+// We had to return the variables in the getters, and set them equal to a value in the setters
 int Pirate::getStrength()
 {
     return strength;
