438f7c6c6b280af91c1f48ebe9e45837c10ef839
make3831 <117610230+make3831@users.noreply.github.com>
2022-11-10 13:37:04 -0700

    Updated pirate.h with comments

--- Pirate.h
+++ Pirate.h
@@ -2,18 +2,22 @@
 #define PIRATE_H
 
 #include <iostream>
-#include "Player.h"
 using namespace std;
 
 class Pirate{
+    // We had to declare the variables we declared prior as a public so they could be manipulated by anyone 
+    // We set some variables as getters to retrieve a variable's value
+    // We set the other variables as setters to set the variables value in my function and these are voided, so they will not return
     private:
         int strength;
-        int postionR;
-        int postionC;
+        int positionR;
+        int positionC;
         bool beenFound;
     public:
-        Pirate();
-        Pirate(int strength, int postionR, int postionC);
+        // We used getters and setters in our h file
+        // Declared the variables inside the parameter
+        Pirate(); // default constructor
+        Pirate(int strength, int positionR, int positionC);
 
         int getStrength();
         
