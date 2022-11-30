bd77558a8a48eb57a07b636fc3508c02239ea5b2
Sophia Soka <112036430+SophiaSoka@users.noreply.github.com>
2022-11-30 13:37:57 -0700

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 // default constructor to set the initial values of time and name
 Person::Person(){
-    time = 0;
+    moves = 0;
     name = "";
 }
 
 // Here we declared the variables using t and n to have them hold values
-Person::Person(int t, string n){
-    time = t;
+Person::Person(int m, string n){
+    moves = t;
     name = n;
 }
 
 // We used getters to retrieve the data
 // We used setters to read the data
-int Person::getTime(){
-    return time;
+int Person::getMoves(){
+    return moves;
 }
-void Person::setTime(){
-    time = t;
+void Person::setMoves(int m){
+    moves = m;
 }
 string Person::getName(){
     return name;
 }
-void Person::setName(){
+void Person::setName(string n){
     name = n;
 }
