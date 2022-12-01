649f992d476997b174e3d56c7e31c7304fb5fccd
make3831 <117610230+make3831@users.noreply.github.com>
2022-11-30 20:41:52 -0700

    Update bigDriver.cpp

--- bigDriver.cpp
+++ bigDriver.cpp
@@ -23,13 +23,27 @@ int main()
     cout << "Answer: " << map.getTrapAt(0).getAnswer() << endl;
     cout << "Hint: " << map.getTrapAt(0).getHint() << endl;
     cout << "Second Trap.. "<< map.getTrapAt(1).getRiddle() << endl;
-    Person p1 = Person("Matty", );
-    Person p2 = Person();
-    Person p3 = Person();
+    Person p1 = Person("Matty", 354, 2);
+    Person p2 = Person("Sophia", 475, 3);
+    Person p3 = Person("Michael", 215, 1);
+    Person p4 = Person("Sarah", 875, 2);
+    Person p5 = Person("Conlan", 153, 1);
+    Person p6 = Person("Roman", 57, 2);
+    Person p7 = Person("Sander", 675, 3);
+    Person p8 = Person("Amber", 712, 3);
+    Person p9 = Person("Laya", 347, 3);
+    Person p10 = Person("Logan", 567, 3);
 
     map.addPerson(p1);
     map.addPerson(p2);
     map.addPerson(p3);
+    map.addPerson(p4);
+    map.addPerson(p5);
+    map.addPerson(p6);
+    map.addPerson(p7);
+    map.addPerson(p8);
+    map.addPerson(p9);
+    map.addPerson(p10);
 
     map.displayMap();
 }
